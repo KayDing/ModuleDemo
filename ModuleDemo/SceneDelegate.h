fe676901d388c5b8ab7be6428bebbead1460b778
DingLei <dinglei.hikari@bytedance.com>
2020-07-28 11:11:16 +0800

    Initial Commit

---
+++ ModuleDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ModuleDemo
+//
+//  Created by 丁磊 on 2020/7/28.
+//  Copyright © 2020 DingLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ModuleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleDemo
+//
+//  Created by 丁磊 on 2020/7/28.
+//  Copyright © 2020 DingLei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
